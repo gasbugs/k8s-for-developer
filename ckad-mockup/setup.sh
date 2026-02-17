e74e5a33e43b9d39109b70fcd8a6ee353d30b740
Ilsun <ilsunchoi@cloudsecuritylab.co.kr>
2026-02-17 14:33:45 +0900

    Add Rancher Local Path Provisioner to setup.sh

--- ckad-mockup/setup.sh
+++ ckad-mockup/setup.sh
@@ -7,6 +7,9 @@ kind create cluster --config kind-mockup-config.yaml
 echo "2. Installing Cilium..."
 cilium install --version 1.18.4
 
+echo "Installing Rancher Local Path Provisioner..."
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.30/deploy/local-path-storage.yaml
+
 echo "3. Installing Traefik..."
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
